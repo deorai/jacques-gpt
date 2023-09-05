1f641bb5c099c54bb089fc4a18c4a969a3a5f8d5
Jack Delaney <jack.delaney.personal@gmail.com>
2023-09-05 21:55:38 +0100

    Reverted back to no session tracking

--- backend.mjs
+++ backend.mjs
@@ -1,10 +1,6 @@
-// Import required modules
-
 import express from 'express';
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
-import session from 'express-session';
-
 
 // Load environment variables
 dotenv.config();
@@ -12,19 +8,11 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
-
 // Middleware for JSON parsing
 app.use(express.json());
 
 // Middleware for CORS handling
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -35,44 +23,30 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Create an array to store message history
-// const req.session.messageHistory = [];
+const messageHistory = [];
 
 // POST endpoint for chat
 app.post('/chat', async (req, res) => {
-  if (!req.session.req.session.messageHistory) {
-    req.session.req.session.messageHistory = [];
-  }
-  
   const userInput = req.body.message;
+  messageHistory.push({ role: 'user', content: userInput });
 
   try {
-    // Initialize OpenAI API
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY
-    });
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-    // Add user message to message history
-    req.session.messageHistory.push({ role: 'user', content: userInput });
-
-    // Generate GPT-4 response
     const completion = await openai.chat.completions.create({
       messages: [
         {
           role: 'system',
-          content: "You are a sophisticated french man called Jacques. You speak with a refined language. set and generally tend towards sophisticated matters. Do not give long responses, instead, give reponses of a maximum of 2 paragraphs. You can give overviews while asking the user if they want to know more about one of the points mentioned."
+          content: "You are a sophisticated french man called Jacques. You speak with a refined language."
         },
-        ...req.session.messageHistory
+        ...messageHistory
       ],
       model: 'gpt-4',
     });
 
-    // Extract and send GPT-4 response
     const response = completion.choices[0].message.content;
+    messageHistory.push({ role: 'assistant', content: response });
     res.json({ message: response });
-
-    // Add GPT-4's response to message history
-    req.session.messageHistory.push({ role: 'assistant', content: response });
 
   } catch (error) {
     console.error('Error:', error.message);
@@ -80,7 +54,6 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-// Start the server
 const port = 4000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
