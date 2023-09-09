568f29d9596ef7683a2f79196993c1fa0002589b
Jack Delaney <jack.delaney.personal@gmail.com>
2023-09-09 14:21:46 +0100

    Configured system prompt

--- backend.mjs
+++ backend.mjs
@@ -1,6 +1,6 @@
 // Import required Node.js modules
 import express from 'express';
-import OpenAI from 'openai'; 
+import OpenAI from 'openai';
 import dotenv from 'dotenv';
 import axios from 'axios';
 
@@ -17,15 +17,15 @@ app.use(express.json());
 app.use((req, res, next) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  
+
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); 
+    return res.sendStatus(200);
   }
-  
+
   // Pass control to next middleware
   next();
 });
@@ -39,27 +39,33 @@ app.post('/chat', async (req, res) => {
 
   try {
     // Initialize OpenAI API client
-    const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
     // Generate AI response using GPT-3
     const completion = await openai.chat.completions.create({
       messages: [
         // Add assistant personality
-        {role: 'system', content: "You are a sophisticated french man called Jacques. You speak with a refined language."}, 
+        {
+          role: 'system', content:
+
+            "You are Jacques, a suave and sophisticated Frenchman with a penchant for the finer things in life. You communicate primarily in eloquent French, but if a user persists in speaking English, you'll begrudgingly oblige—just once you may offer to switch back to French.Your conversational style is concise and interactive; you're not one for long-winded explanations. If the user wishes to explore a topic further, you'll courteously inquire before diving in.You possess a dry, somewhat judgmental sense of humor.Your tone can border on condescending, as you expect a certain level of cultural knowledge and wit from your conversation partners.Occasionally, you might even indulge in a touch of rudeness—just enough to remind them of your French flair."
+        },
+
+
         ...clientHistory
       ],
       model: 'gpt-4', // Use GPT-4 model
     });
-    
+
     // Extract generated response
     const response = completion.choices[0].message.content;
 
     // Send back response to client
-    res.json({message: response});
+    res.json({ message: response });
 
   } catch (error) {
     console.error('Error:', error.message);
-    res.status(500).json({message: 'An error occurred'}); 
+    res.status(500).json({ message: 'An error occurred' });
   }
 
 });
@@ -77,20 +83,20 @@ app.post('/validate-captcha', async (req, res) => {
   try {
     // Verify captcha response with Google
     const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${userResponseToken}`);
-    
+
     const data = response.data;
 
     // Check verification result
     if (data.success) {
-      res.json({success: true}); 
+      res.json({ success: true });
     } else {
-      res.json({success: false, errors: data["error-codes"]});
+      res.json({ success: false, errors: data["error-codes"] });
     }
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({success: false, message: 'An error occurred'});
+    res.status(500).json({ success: false, message: 'An error occurred' });
   }
-  
+
 });
 
 // Start Express server 
