129d96f93ba35540c52aa134da4c7fba5a788525
Jack Delaney <jack.delaney.personal@gmail.com>
2023-09-24 11:09:54 +0100

    Updated ports to heroku

--- backend.mjs
+++ backend.mjs
@@ -102,7 +102,7 @@ app.post('/validate-captcha', async (req, res) => {
 });
 
 // Start Express server 
-const port = 4000;
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running in Heroku`);
 });
